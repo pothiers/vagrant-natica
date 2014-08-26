c24c3f482eb40a3d701079b683bb008bb5f1f583
Joshua Hoblitt <jhoblitt@cpan.org>
2014-08-26 16:58:44 -0700

    add redis module

--- Puppetfile
+++ Puppetfile
@@ -2,3 +2,6 @@ forge 'https://forgeapi.puppetlabs.com'
 
 mod 'puppetlabs/stdlib'
 mod 'camptocamp/augeas'
+mod 'stahnma/epel'
+
+mod 'thomasvandoren/redis'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,11 @@
-include stdlib
 include augeas
+# epel is not needed by the puppet redis module but it's nice to have it
+# already configured in the box
+include epel
+
+Class['epel'] -> Package<||>{ provider => 'yum' }
+
+class { 'redis':
+  version        => '2.8.13',
+  redis_password => 'test',
+}
