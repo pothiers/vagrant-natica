8df1edc07f02296d6403d596a6265f45a5411b58
Steve Pothier <pothier@noao.edu>
2016-02-24 10:12:10 -0700

    capture smoke output and email when done

--- create-tada-machines.sh
+++ create-tada-machines.sh
@@ -13,7 +13,6 @@ echo "Starting: $sdate"
 tic=`date +'%s'`
 vagrant destroy -f valley mountain
 time vagrant up valley mountain
-emins=$(((`date +'%s'` - tic)/60))
 
 
 # For workflow: Edit(manifest);Provision, use:
@@ -24,11 +23,12 @@ echo "Try: "                               >> tt.out
 echo "  /opt/tada-cli/scripts/raw_post /data/molest-pair/nhs_2014_n14_299403.fits" >> tt.out
 echo "  vagrant ssh valley -c /sandbox/tada/tests/smoke/smoke.sh" >> tt.out
 
-echo "Done: " `date`
-cat tt.out | mail -s "Try valley! NO CLEAN ($emins)" pothier@email.noao.edu
 
 echo "  vagrant ssh valley -c /sandbox/tada/tests/smoke/smoke.all.sh"
-vagrant ssh valley -c /sandbox/tada/tests/smoke/smoke.all.sh
+vagrant ssh valley -c /sandbox/tada/tests/smoke/smoke.all.sh >> tt.out
 
+echo "Done: " `date`
+emins=$(((`date +'%s'` - tic)/60))
+cat tt.out | mail -s "Vagrant VMs created and smoke run ($emins)" pothier@email.noao.edu
 
 
